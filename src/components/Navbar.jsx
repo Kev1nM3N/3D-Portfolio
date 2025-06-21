e89fbe9667cafcf365eec0e91edd3c759d8be3a7
Kevin <mediaman502@aol.com>
2025-06-20 23:34:20 -0400

    fixed bufferGeometry error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
+import { KDLogo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -41,11 +41,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Kevin &nbsp;
-            <span className='sm:block hidden'> | JS Mastery</span>
-          </p>
+          <img src={KDLogo} alt='logo' className='w-[120px] object-contain invert' />
         </Link>
 
         <ul className='list-none hidden sm:flex flex-row gap-10'>
