8a1e2c2546699b45560a00f9762922b632da7458
Kevin <mediaman502@aol.com>
2025-06-22 11:46:57 -0400

    small changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
               } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(nav.title)}
             >
-              <a href={`#${nav.id}`}>{nav.title}</a>
+              <a className="link__hover-effect" href={`#${nav.id}`}>{nav.title}</a>
             </li>
           ))}
         </ul>
